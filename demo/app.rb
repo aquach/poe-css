7d62b0ca7f620e5638a28a18f3c5c2b31f1a084c
Alex Quach <alexhquach@gmail.com>
2018-01-13 13:23:31 -0800

    Fix demo.

--- demo/app.rb
+++ demo/app.rb
@@ -4,16 +4,18 @@ require 'haml'
 require 'sinatra'
 require 'sinatra/param'
 
-require_relative '../../lib/dependencies'
+require_relative '../lib/dependencies'
 
 get '/compile' do
   param :input, String, required: true
 
-  parsed_clauses = POECSS::Parsing.parse_input(params[:input])
-  result = POECSS::Generator.generate_poe_rules(parsed_clauses)
-
-  content_type 'text/plain'
-  body result
+  begin
+    result = POECSS.compile(params[:input])
+    content_type 'text/plain'
+    body result
+  rescue POECSS::ParseError
+    status 400
+  end
 end
 
 get '/' do
